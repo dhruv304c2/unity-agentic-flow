9150c79560f052d4e6599172dce68d2fff7193ce
siddhant <siddhant2232sharma2232@gmail.com>
2025-10-07 00:45:45 +0530

    All emotes added

--- Assets/SalesPersonTest/ConversationActions/Emote.cs
+++ Assets/SalesPersonTest/ConversationActions/Emote.cs
@@ -2,15 +2,35 @@ using System.Threading;
 using Cysharp.Threading.Tasks;
 using UnityEngine;
 
-public class EmoteActionParams{
-    public float blendShapeBlink;
+public class EmoteActionParams
+{
+	public float blendShapeBlink;
+	public float blendShapeMouthA;
+	public float blendShapeMouthI;
+	public float blendShapeMouthU;
+	public float blendShapeMouthE;
+	public float blendShapeMouthO;
+	public float blendShapeJoy;
+	public float blendShapeAnger;
+	public float blendShapeSorrow;
+	public float blendShapeFun;
+
 }
 
 public class Emote : IAction<DescritpiveContextData>{
     public string ActionId => "emote";
     public string Description => "Make the character perform an emote animation.";
     public string ParamDescription => @"{ 
-	""blendShapeBlink"": float (0.0 to 1.0) 
+	""blendShapeBlink"": float (0.0 to 1.0)
+	""blendShapeMouthA"": float (0.0 to 1.0)
+	""blendShapeMouthI"": float (0.0 to 1.0)
+	""blendShapeMouthU"": float (0.0 to 1.0)
+	""blendShapeMouthE"": float (0.0 to 1.0)
+	""blendShapeMouthO"": float (0.0 to 1.0)
+	""blendShapeJoy"": float (0.0 to 1.0)
+	""blendShapeAnger"": float (0.0 to 1.0)
+	""blendShapeSorrow"": float (0.0 to 1.0)
+	""blendShapeFun"": float (0.0 to 1.0)
     }";
 
     public UniTask<bool> Execute(
--- Assets/SalesPersonTest/ConversationActions/Emoter.cs
+++ Assets/SalesPersonTest/ConversationActions/Emoter.cs
@@ -3,7 +3,18 @@ using UnityEngine;
 public class Emoter : MonoBehaviour{
     [SerializeField] SkinnedMeshRenderer _skinnedMeshRenderer;
 
-    public void Emote(EmoteActionParams param){
-	_skinnedMeshRenderer.SetBlendShapeWeight(0, param.blendShapeBlink * 100f);
+    public void Emote(EmoteActionParams param)
+    {
+        _skinnedMeshRenderer.SetBlendShapeWeight(0, param.blendShapeBlink * 100f);
+        _skinnedMeshRenderer.SetBlendShapeWeight(1, param.blendShapeMouthA * 100f);
+        _skinnedMeshRenderer.SetBlendShapeWeight(2, param.blendShapeMouthI * 100f);
+        _skinnedMeshRenderer.SetBlendShapeWeight(3, param.blendShapeMouthU * 100f);
+        _skinnedMeshRenderer.SetBlendShapeWeight(4, param.blendShapeMouthE * 100f);
+        _skinnedMeshRenderer.SetBlendShapeWeight(5, param.blendShapeMouthO * 100f);
+        _skinnedMeshRenderer.SetBlendShapeWeight(6, param.blendShapeJoy * 100f);
+        _skinnedMeshRenderer.SetBlendShapeWeight(7, param.blendShapeAnger * 100f);
+        _skinnedMeshRenderer.SetBlendShapeWeight(8, param.blendShapeSorrow * 100f);
+         _skinnedMeshRenderer.SetBlendShapeWeight(8, param.blendShapeFun * 100f);
+
     }
 }
