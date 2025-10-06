0a8528de3f3196f2e5b12bebf7dffb4c517b61e3
siddhant <siddhant2232sharma2232@gmail.com>
2025-10-07 02:01:37 +0530

    Typing animation added

--- Assets/SalesPersonTest/ConversationActions/Talker.cs
+++ Assets/SalesPersonTest/ConversationActions/Talker.cs
@@ -2,16 +2,43 @@ using Cysharp.Threading.Tasks;
 using TMPro;
 using UnityEngine;
 
-public class Talker : MonoBehaviour{
+public class Talker : MonoBehaviour {
     [SerializeField] GameObject talkPanel;
     [SerializeField] TextMeshProUGUI talkText;
     [SerializeField] Emoter emoter;
+    [SerializeField] float typeSpeed = 0.05f; // seconds per character
 
-    public async UniTask Talk(string line){
-	talkPanel.SetActive(true);
-	talkText.text = line;
-	await UniTask.WaitForSeconds(line.Length * 0.05f);
-	talkPanel.SetActive(false);
-	emoter.ResetEmote().Forget();
+    bool skip;
+
+    public async UniTask Talk(string line) {
+        talkPanel.SetActive(true);
+        talkText.text = "";
+        skip = false;
+
+        // Run input listener for skip
+        var skipListener = UniTask.Create(async () => {
+            while (!skip) {
+                if (Input.GetMouseButtonDown(0) || Input.GetKeyDown(KeyCode.Space))
+                    skip = true;
+                await UniTask.Yield();
+            }
+        });
+
+        // Typewriter effect
+        for (int i = 0; i < line.Length; i++) {
+            if (skip) {
+                talkText.text = line;
+                break;
+            }
+
+            talkText.text += line[i];
+            await UniTask.Delay((int)(typeSpeed * 1000));
+        }
+
+        // Wait briefly before closing
+        await UniTask.WaitForSeconds(0.5f);
+
+        talkPanel.SetActive(false);
+        emoter.ResetEmote();
     }
 }
